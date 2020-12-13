d211c38e12720af0a3f8c53eb95cf1564e688802
LazyCreeper <40060779+LazyCreeper@users.noreply.github.com>
2020-12-13 15:34:13 +0800

    修改

--- src/main/java/shrbox/github/mcmotd/Serverinfo.java
+++ src/main/java/shrbox/github/mcmotd/Serverinfo.java
@@ -3,9 +3,9 @@ package shrbox.github.mcmotd;
 public class Serverinfo {
     public String status;
     public String motd;
-    public String agreement;
+    public String platform;
     public String version;
-    public String online;
-    public String max;
+    public String players_online;
+    public String players_max;
     public String gamemode;
 }

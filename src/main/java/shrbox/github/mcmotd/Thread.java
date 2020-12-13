89f63fcbd26ddd0643d01d136c044165aa05391a
LazyCreeper <40060779+LazyCreeper@users.noreply.github.com>
2020-12-13 15:35:08 +0800

    修改

--- src/main/java/shrbox/github/mcmotd/Thread.java
+++ src/main/java/shrbox/github/mcmotd/Thread.java
@@ -14,27 +14,27 @@ public class Thread extends java.lang.Thread {
     @Override
     public void run() {
         String msg = e.getMessage().contentToString();
-        String domain = msg.replace("!motdpe", "").toLowerCase().trim();
+        String domain = msg.replace("!motd", "").toLowerCase().trim();
         if (e.getMessage().contentToString().equals("")) {
             e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender()))
                     .plus("请输入一个地址"));
             return;
         }
-        String port = "19132";
+        String port = "25565";
         if (domain.contains(":")) {
             port = msg.split(":")[1];
         }
         String Json = Connection.getURL(domain, port);
         if (Json.equals("")) {
-            e.getGroup().sendMessage(("[MCMotd] 接口无响应"));
+            e.getGroup().sendMessage(("[MCAPI] 接口无响应"));
             return;
         }
         Gson gson = new Gson();
         Serverinfo serverinfo = gson.fromJson(Json, Serverinfo.class);
         if (serverinfo.status.equals("offline")) {
-            e.getGroup().sendMessage("[MCMotd] 服务器不在线");
+            e.getGroup().sendMessage("[MCAPI] 服务器不在线");
             return;
         }
-        e.getGroup().sendMessage("[MCMotd]\nMotd: " + serverinfo.motd + "\n协议版本: " + serverinfo.agreement + "\n游戏版本: " + serverinfo.version + "\n在线: " + serverinfo.online + "/" + serverinfo.max + "\n游戏模式: " + serverinfo.gamemode);
+        e.getGroup().sendMessage("[MCAPI]\nMotd: " + serverinfo.motd + "\n协议版本: " + serverinfo.platform + "\n游戏版本: " + serverinfo.version + "\n在线: " + serverinfo.players_online + "/" + serverinfo.players_max + "\n游戏模式: " + serverinfo.gamemode);
     }
 }
